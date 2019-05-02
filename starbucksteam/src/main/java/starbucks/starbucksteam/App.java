08c66b9ebd9ecc80478627669b5624571003fe8d
SARAH <SARAH@192.168.0.22>
2019-05-01 17:57:39 -0700

    create new starbucks project

---
+++ starbucksteam/src/main/java/starbucks/starbucksteam/App.java
@@ -0,0 +1,13 @@
+package starbucks.starbucksteam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
