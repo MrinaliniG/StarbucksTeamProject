001c81f1dd3f899a12cc227e5cea84d13acc5c1e
SARAH <SARAH@10.251.182.94>
2019-05-11 19:14:13 -0700

    commit

--- starbucksteam/src/main/resources/import.sql
+++ starbucksteam/src/main/resources/import.sql
@@ -3,15 +3,8 @@
 --insert into card (cardid, balance, cardcode, email) values (123456, 8, 781, 1);
 --insert into starbucks_order (order_id, bill_amount, user_id) values (2, 5, 1);
 --insert into starbucks_order (order_id, bill_amount, user_id) values (3, 6, 1);
-
-<<<<<<< HEAD
 --insert into menu(itemName, price, category) values('mocha', 1.50, 'COFFEE');
 --insert into menu(itemName, price, category) values('cappacino', 2.50, 'COFFEE');
 --insert into menu(itemName, price, category) values('cream', 1.25, 'TOPPING');
 --insert into menu(itemName, price, category) values('whipped cream', 1.50, 'TOPPING');
-=======
---insert into Menu(itemName, price, category) values('mocha', 1.50, 'COFFEE');
---insert into Menu(itemName, price, category) values('cappacino', 2.50, 'COFFEE');
---insert into Menu(itemName, price, category) values('cream', 1.25, 'TOPPING');
---insert into Menu(itemName, price, category) values('whipped cream', 1.50, 'TOPPING');
->>>>>>> refs/remotes/origin/master
+
